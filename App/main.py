3c7f6e6046999a6b8313f3c973cdf3d7b95d2791
GautamManoharan <gautam.raw@gmail.com>
2017-07-06 20:48:04 +1000

    separated view to kv file

--- App/main.py
+++ App/main.py
@@ -1,8 +1,8 @@
 #Android app for controlling PSI Goniometers
 
 from kivy.app import App
-from kivy.uix.label import Label
-from kivy.uix.button import Button
+from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
 from kivy.uix.boxlayout import BoxLayout
 
 from kivy.support import install_twisted_reactor
@@ -12,9 +12,12 @@ install_twisted_reactor()
 from twisted.internet.protocol import DatagramProtocol
 from twisted.internet import reactor
 
+from kivy.lang import Builder
+Builder.load_file("pingwindow.kv")
+
 class Pinger(DatagramProtocol):
-    def __init__(self, App):
-        self.app = App
+    def __init__(self, _window):
+        self.app = _window
 
     def startProtocol(self):
         self.transport.setBroadcastAllowed(True)
@@ -23,31 +26,27 @@ class Pinger(DatagramProtocol):
         pingMsg = "PING:"
         self.transport.write(pingMsg,('255.255.255.255',9999))#('192.168.1.255',9999))
         #('<broadcast>',9999))
-        self.app.label.text += "{}\n".format(pingMsg)
+        self.app.label.text = ""
 
     def datagramReceived(self, data, (host, port)):
         msg = "received %r from %s:%d" % (data, host, port)
         if data[:4] == "PONG":
             self.app.label.text += "{}\n".format(msg)
 
-class PyPSIApp(App):
-    label = None
-    button = None
-    pinger = None
+class pingWindow(BoxLayout):
+    label = ObjectProperty(None)
+    button = ObjectProperty(None)
 
-    def setup_gui(self):
-        layout = BoxLayout(orientation='vertical')
-        self.button = Button(text='Send Ping Msg')
-        self.button.bind(on_press=self.pinger.sendPing)
-        self.label = Label(text='connecting...\n')
-        layout.add_widget(self.button)
-        layout.add_widget(self.label)
-        return layout
+class PyPSIApp(App):
+    pinger = None
+    pingwindow = None
 
     def build(self):
-        self.pinger = Pinger(self)
+        self.pingwindow = pingWindow()
+        self.pinger = Pinger(self.pingwindow)
+        self.pingwindow.button.bind(on_press=self.pinger.sendPing)
         reactor.listenUDP(0,self.pinger)
-        return self.setup_gui()
+        return self.pingwindow
 
 if __name__ == '__main__':
     PyPSIApp().run()
