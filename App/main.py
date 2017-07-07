185478453fac180338f5a24feb70a0876d20d979
GautamManoharan <gautam.raw@gmail.com>
2017-07-07 22:47:45 +1000

    screenmanager added

--- App/main.py
+++ App/main.py
@@ -1,21 +1,31 @@
 #Android app for controlling PSI Goniometers
 from kivy.app import App
-from pingwindow import Pinger, pingWindow
+from kivy.uix.screenmanager import ScreenManager
 
 from kivy.support import install_twisted_reactor
 install_twisted_reactor()
+
 from twisted.internet import reactor
+from pingwindow import Pinger, pingWindow
+
+class ScreenMan(ScreenManager):
+    pass
 
 class PyPSIApp(App):
+    screenmanager = None
     pinger = None
     pingwindow = None
 
     def build(self):
+        self.screenmanager = ScreenMan()
+
         self.pingwindow = pingWindow()
-        self.pinger = Pinger(self.pingwindow)
+        self.pinger = Pinger(self.pingwindow, self.screenmanager)
         self.pingwindow.button.bind(on_press=self.pinger.sendPing)
         reactor.listenUDP(0,self.pinger)
-        return self.pingwindow
+
+        self.screenmanager.get_screen("findhostswindow").add_widget(self.pingwindow)
+        return self.screenmanager
 
 if __name__ == '__main__':
     PyPSIApp().run()
--- App/pingwindow.py
+++ App/pingwindow.py
@@ -1,13 +1,16 @@
 from twisted.internet.protocol import DatagramProtocol
 from kivy.uix.button import Button
+from kivy.properties import ObjectProperty
 from kivy.uix.boxlayout import BoxLayout
 from kivy.lang import Builder
 Builder.load_file("pingwindow.kv")
 
 class Pinger(DatagramProtocol):
     pingwindow=None
+    scrman=None
 
-    def __init__(self, _window):
+    def __init__(self, _window, _scrman):
+         self.scrman = _scrman
          self.pingwindow = _window
 
     def startProtocol(self):
@@ -19,9 +22,14 @@ class Pinger(DatagramProtocol):
          self.pingwindow.hostsContainer.clear_widgets()
 
     def datagramReceived(self, data, (host, port)):
-         msg = "%s" % host
+         btn=Button(text=host)
+         btn.bind(on_press=self.nextscreen)
          if data[:4] == "PONG":
-             self.pingwindow.hostsContainer.add_widget(Button(text=msg))
+             self.pingwindow.hostsContainer.add_widget(btn)
+
+    def nextscreen(self, args):
+        self.transport.loseConnection()
+        self.scrman.current="findcontrollerswindow"
 
 class pingWindow(BoxLayout):
     pass
